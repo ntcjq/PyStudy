7ea1c954c7c9ac19b6729bae30ff9bde90251fa8
Sea <754119493@qq.com>
2023-07-24 16:59:59 +0800

    python study

--- crud/mysql.py
+++ crud/mysql.py
@@ -1,61 +1,99 @@
 import pymysql
 
 
-def crud():
+def getConnect():
     # 打开数据库连接
-    db = pymysql.connect(host='localhost',
-                         port=3306,
-                         user='root',
-                         password='Cjq51729',
-                         db='springboot')
+    conn = pymysql.connect(host='localhost',
+                           port=3306,
+                           user='root',
+                           password='Cjq51729',
+                           db='springboot')
+    return conn
 
+
+def printVersion(conn):
     # 使用 cursor() 方法创建一个游标对象 cursor
-    cursor = db.cursor()
+    with conn.cursor() as cursor:
+        # 使用 execute()  方法执行 SQL 查询
+        cursor.execute("SELECT VERSION()")
+        # 使用 fetchone() 方法获取单条数据.
+        data = cursor.fetchone()
+        print("Database version : %s " % data)
 
-    # 使用 execute()  方法执行 SQL 查询
-    cursor.execute("SELECT VERSION()")
 
-    # 使用 fetchone() 方法获取单条数据.
-    data = cursor.fetchone()
+def createTable(conn):
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    with conn.cursor() as cursor:
+        # 使用 execute() 方法执行 SQL，如果表存在则删除
+        cursor.execute("DROP TABLE IF EXISTS Employee")
+        # 使用预处理语句创建表
+        createSql = """
+                CREATE TABLE Employee (
+                FIRST_NAME  CHAR(20) NOT NULL,
+                LAST_NAME  CHAR(20),
+                AGE INT,  
+                SEX CHAR(1),
+                INCOME FLOAT )
+                """
+        cursor.execute(createSql)
 
-    print("Database version : %s " % data)
 
-    # 使用 execute() 方法执行 SQL，如果表存在则删除
-    cursor.execute("DROP TABLE IF EXISTS Employee")
-
-    # 使用预处理语句创建表
-    createSql = """
-            CREATE TABLE Employee (
-            FIRST_NAME  CHAR(20) NOT NULL,
-            LAST_NAME  CHAR(20),
-            AGE INT,  
-            SEX CHAR(1),
-            INCOME FLOAT )
+def insert(conn):
+    with conn.cursor() as cursor:
+        # SQL 插入语句
+        sql = """
+            INSERT INTO Employee(FIRST_NAME,LAST_NAME, AGE, SEX, INCOME) VALUES 
+            ('Mac', 'Mohan', 20, 'F', 2000),
+            ('Jack', 'Moyu', 21, 'M', 5000),
+            ('Jone', 'Mona', 23, 'M', 3000),
+            ('Amy', 'Mobi', 31, 'M', 500);
             """
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 提交到数据库执行
+            conn.commit()
+        except:
+            print("新增异常")
+            # 如果发生错误则回滚
+            conn.rollback()
 
-    cursor.execute(createSql)
 
-    # SQL 插入语句
-    sql = """
-         INSERT INTO EMPLOYEE(FIRST_NAME,LAST_NAME, AGE, SEX, INCOME) VALUES 
-         ('Mac', 'Mohan', 20, 'F', 2000),
-         ('Jack', 'Moyu', 21, 'M', 5000),
-         ('Jone', 'Mona', 23, 'M', 3000),
-         ('Amy', 'Mobi', 31, 'M', 500);
-         """
-    try:
-        # 执行sql语句
-        cursor.execute(sql)
-        # 提交到数据库执行
-        db.commit()
-    except:
-        # 如果发生错误则回滚
-        db.rollback()
+def update(conn):
+    with conn.cursor() as cursor:
+        try:
+            affected_rows = cursor.execute(
+                'update `Employee` set INCOME = 10000 where `FIRST_NAME`= "Amy"'
+            )
+            if affected_rows == 1:
+                print('更新用户Amy成功!')
+            # 提交到数据库执行
+            conn.commit()
+        except:
+            print("更新异常")
+            # 如果发生错误则回滚
+            conn.rollback()
 
-    # SQL 查询语句
-    sql = "SELECT * FROM EMPLOYEE \
-         WHERE INCOME > '%d'" % (1000)
-    try:
+
+def delete(conn):
+    with conn.cursor() as cursor:
+        try:
+            affected_rows = cursor.execute(
+                'delete from `Employee` where `FIRST_NAME`="Mac"')
+            if affected_rows == 1:
+                print('删除用户Mac成功!')
+            # 提交到数据库执行
+            conn.commit()
+        except:
+            print("删除异常")
+            # 如果发生错误则回滚
+            conn.rollback()
+
+
+def select(conn):
+    with conn.cursor() as cursor:
+        # SQL 查询语句
+        sql = "SELECT * FROM Employee"
         # 执行SQL语句
         cursor.execute(sql)
         # 获取所有记录列表
@@ -68,14 +106,26 @@ def crud():
             income = row[4]
             # 打印结果
             print ("fname=%s,lname=%s,age=%d,sex=%s,income=%d" % \
-                  (fname, lname, age, sex, income ))
-    except:
-        print("Error: unable to fecth data")
-
-    # 关闭数据库连接
-    db.close()
+                (fname, lname, age, sex, income ))
 
 
 if __name__ == "__main__":
-    crud()
-    print("执行结束")
+    try:
+        conn = getConnect()
+        printVersion(conn)
+        createTable(conn)
+        insert(conn)
+        select(conn)
+        print("=" * 30)
+        update(conn)
+        select(conn)
+        print("=" * 30)
+        delete(conn)
+        select(conn)
+    except RuntimeError as err:
+        print(err)
+        print("出现异常")
+    finally:
+        #关闭连接释放资源
+        conn.close()
+        print("执行结束")
--- crud/sqlite.py
+++ crud/sqlite.py
@@ -1,68 +1,106 @@
 import sqlite3
-import pytz
-import dateutil
+from contextlib import closing
 from prettytable import PrettyTable
 
 
-def crud():
+def getConnect():
     # 连接到数据库（如果不存在则会创建新的数据库文件）
     conn = sqlite3.connect('file/example.db')
+    return conn
 
-    # 创建游标对象
-    cursor = conn.cursor()
 
-    # 创建表
-    cursor.execute('''CREATE TABLE IF NOT EXISTS students
+def createTable(conn):
+    with closing(conn.cursor()) as cursor:
+        # 创建表
+        cursor.execute('''CREATE TABLE IF NOT EXISTS students
                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT,
                     age INTEGER)''')
 
-    # 插入数据
-    cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
-                   ('Alice', 18))
-    cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
-                   ('Tom', 18))
-    cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
-                   ('Bob', 22))
-    cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
-                   ('Charlie', 19))
 
-    # 提交事务
-    conn.commit()
+def insert(conn):
+    # 创建游标对象
+    with closing(conn.cursor()) as cursor:
+        try:
+            # 插入数据
+            cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
+                           ('Alice', 18))
+            cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
+                           ('Tom', 18))
+            cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
+                           ('Bob', 22))
+            cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)",
+                           ('Charlie', 19))
+            # 提交事务
+            conn.commit()
+        except:
+            conn.rollback()
 
-    # 查询数据
-    cursor.execute("SELECT * FROM students")
-    rows = cursor.fetchall()
-    for row in rows:
-        print("query", row)
 
-    # 更新数据
-    cursor.execute("UPDATE students SET age = ? WHERE name = ?", (21, 'Alice'))
-    conn.commit()
+def update(conn):
+    # 创建游标对象
+    with closing(conn.cursor()) as cursor:
+        try:
+            # 更新数据
+            cursor.execute("UPDATE students SET age = ? WHERE name = ?",
+                           (21, 'Alice'))
+            # 提交事务
+            conn.commit()
+        except:
+            conn.rollback()
 
-    # 删除数据
-    cursor.execute("DELETE FROM students WHERE age > ?", (20, ))
-    conn.commit()
 
-    # 查询删除后的数据
-    cursor.execute("SELECT * FROM students")
-    rows = cursor.fetchall()
-    for row in rows:
-        print("delete query", row)
+def delete(conn):
+    # 创建游标对象
+    with closing(conn.cursor()) as cursor:
+        try:
+            # 删除数据
+            cursor.execute("DELETE FROM students WHERE age > ?", (20, ))
+            # 提交事务
+            conn.commit()
+        except:
+            conn.rollback()
 
-    # prettyTbale
-    table = PrettyTable()
-    table.title = "学生信息"
-    table.field_names = ["姓名", "年龄"]
-    for row in rows:
-        table.add_row([row[0], row[1]])
-    print(table)
 
-    # 关闭游标和连接
-    cursor.close()
-    conn.close()
+def deleteAll(conn):
+    # 创建游标对象
+    with closing(conn.cursor()) as cursor:
+        try:
+            # 删除数据
+            cursor.execute("DELETE FROM students")
+            # 提交事务
+            conn.commit()
+        except:
+            conn.rollback()
+
+
+def select(conn):
+    with closing(conn.cursor()) as cursor:
+        # 查询数据
+        cursor.execute("SELECT * FROM students")
+        rows = cursor.fetchall()
+        # prettyTbale
+        table = PrettyTable()
+        table.title = "学生信息"
+        table.field_names = ["ID", "姓名", "年龄"]
+        for row in rows:
+            table.add_row([row[0], row[1], row[2]])
+        print(table)
 
 
 if __name__ == "__main__":
-    crud()
+    try:
+        conn = getConnect()
+        createTable(conn)
+        deleteAll(conn)
+        insert(conn)
+        select(conn)
+        update(conn)
+        select(conn)
+        delete(conn)
+        select(conn)
+    except RuntimeError as err:
+        print(err)
+    finally:
+        conn.close()
     print("执行结束")
