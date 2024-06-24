024bd13c12599915fea610d217c2e1a92f256856
Sea <754119493@qq.com>
2024-06-24 16:19:47 +0800

    song download

--- webSpider/songDL.py
+++ webSpider/songDL.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import json
+import random
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
@@ -10,60 +11,105 @@ headers = {
 domain = "https://www.gequbao.com"
 
 
-def search(songName):
-
-    searchUrl = domain + "/s/"
+def search(songName, author):
     # songName = input("请输入歌曲名称：")
-    searchUrl += songName
+    searchUrl = f"{domain}/s/{songName}"
     response = requests.get(searchUrl, headers=headers)
     if response.status_code == 200:
         # 解析HTML内容
         bs = BeautifulSoup(response.text, features="html.parser")
         card = bs.find("div", class_="card-text")
         divs = card.find_all("div", class_="row")
+        mp3Id = None
+        songNameTemp = None
+        authorTemp = None
+        fileName = None
         for index, div in enumerate(divs):
-            if index == 1:
-                songName = ""
-                mp3Id = None
+            if index != 0:
                 a = div.find("a", class_="text-primary font-weight-bold")
                 authorDiv = div.find("div", class_="text-success")
-                if authorDiv is not None:
-                    author = authorDiv.text.strip() + "-"
-                    songName = author
                 if a is not None:
-                    songName += a.text.strip() + ".mp3"
+                    songNameTemp = a.text.strip()
                     mp3Id = a.attrs["href"].split("/")[2]
-                getDownloadUrl(mp3Id, songName)
-
+                if authorDiv is not None:
+                    authorTemp = authorDiv.text.strip()
+                if authorTemp == author and songNameTemp == songName:
+                    fileName = f"{author}-{songName}.mp3"
+                    break
+        getDownloadUrl(mp3Id, fileName)
     else:
-        print("无法搜索到该歌曲。状态码:", response.status_code)
+        print(f"请求异常:{author}-{songName}")
 
 
-def getDownloadUrl(mp3Id, songName):
-    play_url = domain + "/api/play_url?id=" + mp3Id + "&json=1"
+def searchByAuthor(author):
+    searchUrl = f"{domain}/s/{author}"
+    response = requests.get(searchUrl, headers=headers)
+    if response.status_code == 200:
+        # 解析HTML内容
+        bs = BeautifulSoup(response.text, features="html.parser")
+        card = bs.find("div", class_="card-text")
+        divs = card.find_all("div", class_="row")
+        mp3Id = None
+        songNameTemp = None
+        authorTemp = None
+        fileName = None
+        for index, div in enumerate(divs):
+            if index != 0:
+                a = div.find("a", class_="text-primary font-weight-bold")
+                authorDiv = div.find("div", class_="text-success")
+                if a is not None:
+                    songNameTemp = a.text.strip()
+                    mp3Id = a.attrs["href"].split("/")[2]
+                if authorDiv is not None:
+                    authorTemp = authorDiv.text.strip()
+                if authorTemp != author:
+                    continue
+            fileName = f"{author}-{songNameTemp}.mp3"
+            getDownloadUrl(mp3Id, fileName)
+            delay()
+    else:
+        print(f"请求异常:{author}-{songName}")
+
+
+def getDownloadUrl(mp3Id, fileName):
+    play_url = f"{domain}/api/play_url?id={mp3Id}&json=1"
     response = requests.get(play_url, headers=headers, timeout=10)
     # 检查请求是否成功
     if response.status_code == 200:
         obj = json.loads(response.content)
         downloadUrl = obj["data"]["url"]
-        print(songName, "", downloadUrl)
-        download(downloadUrl, songName)
+        if downloadUrl is not None:
+            download(downloadUrl, fileName)
     else:
-        print("无法下载歌曲。状态码:", response.status_code)
+        print(f"请求异常:{fileName}")
 
 
-def download(downloadUrl, name):
+def download(downloadUrl, fileName):
     response = requests.get(downloadUrl, headers=headers, timeout=10)
     # 检查请求是否成功
     if response.status_code == 200:
         # 打开一个文件来保存下载的内容
-        with open(name, "wb") as file:
+        with open(f"C:/Users/Sea/Downloads/music/{fileName}", "wb") as file:
             file.write(response.content)
-        print(name + " 下载成功！")
+        print(f"下载成功:{fileName}")
     else:
-        print("无法下载歌曲。状态码:", response.status_code)
+        print(f"下载失败:{fileName}")
+
+
+def delay():
+    delay = random.uniform(1, 5)  # 生成1到5的随机数
+    time.sleep(delay)
 
 
 if __name__ == "__main__":
-    author = "多余的解释"
-    search(author)
+    # 按歌名下载
+    playList = ["断桥残雪-许嵩", "多余的解释-许嵩"]
+    for song in playList:
+        info = song.split("-")
+        songName = info[0]
+        author = info[1]
+        search(songName, author)
+        delay()
+
+    # 按歌手下载
+    # searchByAuthor("许嵩")
